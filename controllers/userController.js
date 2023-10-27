a1d76295c5a2f984e80d89cc5e616fc8a82cb556
Manoj_Pant <manojpant097@gmail.com>
2023-10-27 13:30:14 +0530

    bug found in usersController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,104 +1,125 @@
 const User = require('../models/userSchema');
 const Student = require('../models/studentSchema');
+const fs = require('fs');
 const fastcsv = require('fast-csv');
-const path = require('path');
-const fs = require('fs/promises'); // Use the Promises version of the fs module
 
-// Render sign-up page
-exports.signup = function (req, res) {
+// render sign up page
+module.exports.signup = function (req, res) {
 	if (req.isAuthenticated()) {
 		return res.redirect('back');
 	}
 	res.render('signup');
 };
 
-// Render sign-in page
-exports.signin = function (req, res) {
+// render sign in page
+module.exports.signin = function (req, res) {
 	if (req.isAuthenticated()) {
 		return res.redirect('back');
 	}
 	res.render('signin');
 };
 
-
-exports.createSession = function (req, res) {
+// create session
+module.exports.createSession = function (req, res) {
 	console.log('Session created successfully');
 	return res.redirect('/');
 };
 
-// Sign out
-exports.signout = function (req, res) {
-	req.logout(); // You don't need to pass a callback function here
+// signout
+module.exports.signout = function (req, res) {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+	});
 	return res.redirect('/users/signin');
 };
 
-// Create user
-exports.createUser = async function (req, res) {
+// create user
+module.exports.createUser = async function (req, res) {
 	const { name, email, password, confirmPassword } = req.body;
 	try {
 		if (password !== confirmPassword) {
-			console.log(`Passwords didn't match`);
+			console.log(`Passwords dont match`);
 			return res.redirect('back');
 		}
-
 		const user = await User.findOne({ email });
 
 		if (user) {
 			console.log(`Email already exists`);
 			return res.redirect('back');
-	}
+		}
 
-	const newUser = new User({
-		name,
-		email,
-		password,
-	});
+		const newUser = await User.create({
+			name,
+			email,
+			password,
+		});
 
-	await newUser.save();
+		await newUser.save();
 
-	console.log('User created successfully');
-	return res.redirect('/users/signin');
+		if (!newUser) {
+			console.log(`Error in creating user`);
+			return res.redirect('back');
+		}
+
+		return res.redirect('/users/signin');
 	} catch (error) {
-		console.error(`Error in creating user: ${error}`);
+		console.log(`Error in creating user: ${error}`);
 		res.redirect('back');
 	}
 };
 
-// Download report
-exports.downloadCsv = async function (req, res) {
+// download report
+module.exports.downloadCsv = async function (req, res) {
 	try {
 		const students = await Student.find({});
-		const filePath = path.join(__dirname, '../report/data.csv');
 
-		const ws = fs.createWriteStream(filePath);
-		const csvStream = fastcsv.format({ headers: true });
+		let data = '';
+		let no = 1;
+		let csv = 'S.No, Name, Email, College, Placemnt, Contact Number, Batch, DSA Score, WebDev Score, React Score, Interview, Date, Result';
 
-		csvStream.pipe(ws);
+		for (let student of students) {
+			data =
+				no +
+				',' +
+				student.name +
+				',' +
+				student.email +
+				',' +
+				student.college +
+				',' +
+				student.placement +
+				',' +
+				student.contactNumber +
+				',' +
+				student.batch +
+				',' +
+				student.dsa +
+				',' +
+				student.webd +
+				',' +
+				student.react;
 
-		students.forEach((student) => {
-			csvStream.write({
-				'S.No': student.sno,
-				Name: student.name,
-				Email: student.email,
-				College: student.college,
-				Placement: student.placement,
-				'Contact Number': student.contactNumber,
-				Batch: student.batch,
-				'DSA Score': student.dsa,
-				'WebDev Score': student.webd,
-				'React Score': student.react,
-				...student.interviews,
-			});
+			if (student.interviews.length > 0) {
+				for (let interview of student.interviews) {
+					data += ',' + interview.company + ',' + interview.date.toString() + ',' + interview.result;
+				}
+			}
+			no++;
+			csv += '\n' + data;
+		}
+
+		const dataFile = fs.writeFile('report/data.csv', csv, function (error, data) {
+			if (error) {
+				console.log(error);
+				return res.redirect('back');
+			}
+			console.log('Report generated successfully');
+			return res.download('report/data.csv');
 		});
-
-		csvStream.end();
-
-		res.setHeader('Content-Disposition', `attachment; filename=${path.basename(filePath)}`);
-		res.setHeader('Content-Type', 'text/csv');
-
-		fs.createReadStream(filePath).pipe(res);
 	} catch (error) {
-		console.error(`Error in downloading file: ${error}`);
+		console.log(`Error in downloading file: ${error}`);
 		return res.redirect('back');
 	}
 };
