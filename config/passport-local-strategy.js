659979f736fec353ab20c1df8a23c95ddfe22e8c
Manoj_Pant <manojpant097@gmail.com>
2023-10-27 13:40:43 +0530

    changed some files to resolve bug

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -3,13 +3,15 @@ const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/userSchema');
 
-const local = new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+const local = new LocalStrategy({ usernameField: 'email' }, async function (email, password, done) {
   try {
     const user = await User.findOne({ email });
+
     if (!user || !user.isPasswordCorrect(password)) {
       console.log('Invalid Username/Password');
       return done(null, false);
     }
+
     return done(null, user);
   } catch (error) {
     console.log(`Error in finding user: ${error}`);
@@ -19,19 +21,15 @@ const local = new LocalStrategy({ usernameField: 'email' }, async (email, passwo
 
 passport.use('local', local);
 
-// Serialize user
-passport.serializeUser((user, done) => {
+// serialize user
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-// Deserialize user
-passport.deserializeUser(async (id, done) => {
+// deserialize user
+passport.deserializeUser(async function (id, done) {
   try {
     const user = await User.findById(id);
-    if (!user) {
-      console.log('Error in finding user--> Passport');
-      return done(null, false);
-    }
     return done(null, user);
   } catch (err) {
     console.log('Error in finding user--> Passport');
@@ -39,16 +37,16 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-// Check if the user is authenticated
-passport.checkAuthentication = (req, res, next) => {
+// check if user is authenticated
+passport.checkAuthentication = function (req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   return res.redirect('/users/signin');
 };
 
-// Set the authenticated user for views
-passport.setAuthenticatedUser = (req, res, next) => {
+// set authenticated user for views
+passport.setAuthenticatedUser = function (req, res, next) {
   if (req.isAuthenticated()) {
     res.locals.user = req.user;
   }
